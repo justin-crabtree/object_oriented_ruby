defdb22c54e0a688dfef6ebd76553872ab3e7db8
crabtree <justin.kyle.crabtree@gmail.com>
2019-08-20 15:20:39 -0400

    changed hash into class

--- store_item.rb
+++ store_item.rb
@@ -1,10 +1,54 @@
-item1 = {name: "milk", color: "white", price: 3.00}
-item2 = {name: "orange", color: "orange", price: 0.50}
-item3 = {name: "battle axe", color: "red", price: 10}
+# item1 = {name: "milk", color: "white", price: 3.00}
+# item2 = {name: "orange", color: "orange", price: 0.50}
+# item3 = {name: "battle axe", color: "red", price: 10}
 
-item3 = {"name" => "milk", "color" => "white", "price" => 3.00}
-item4 = {"name" => "orange", "color" => "orange", "price" => 0.50}
-item5 = {"name" => "battle axe", "color" => "red", "price" => 10}
+# item3 = {"name" => "milk", "color" => "white", "price" => 3.00}
+# item4 = {"name" => "orange", "color" => "orange", "price" => 0.50}
+# item5 = {"name" => "battle axe", "color" => "red", "price" => 10}
 
 
-puts item1
+class StoreItem
+
+  def initialize(name, color, price)
+    @name = name
+    @color = color
+    @price = price
+  end
+
+  def name
+    @name
+  end
+
+  def name=(input_name)
+    @name = input_name
+  end
+
+  def color
+    @color
+  end
+
+  def color=(input_color)
+    @color = input_color
+  end
+
+  def price
+    @price
+  end
+
+  def price=(input_price)
+    @price = input_price 
+  end
+
+end
+
+
+item1 = StoreItem.new("milk", "white", 3.00)
+item2 = StoreItem.new("orange", "orange", 0.50)
+item3 = StoreItem.new("battle axe", "red", 10)
+
+puts item3.name
+puts item3.color
+puts item3.price
+
+
+
