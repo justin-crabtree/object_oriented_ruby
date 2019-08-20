bf75ebfda045e0095436c15a937c17e0b8a04291
crabtree <justin.kyle.crabtree@gmail.com>
2019-08-20 14:15:12 -0400

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,10 @@
+item1 = {name: "milk", color: "white", price: 3.00}
+item2 = {name: "orange", color: "orange", price: 0.50}
+item3 = {name: "battle axe", color: "red", price: 10}
+
+item3 = {"name" => "milk", "color" => "white", "price" => 3.00}
+item4 = {"name" => "orange", "color" => "orange", "price" => 0.50}
+item5 = {"name" => "battle axe", "color" => "red", "price" => 10}
+
+
+puts item1
